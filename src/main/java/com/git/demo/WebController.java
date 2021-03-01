cf8867ef6dd88e190c278a51af6a6cde284ff3cc
i060653 <trinoy.hazarika@sap.com>
2021-03-01 10:32:28 -0800

    ping-initial

---
+++ src/main/java/com/git/demo/WebController.java
@@ -0,0 +1,15 @@
+package com.git.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+
+
+}
